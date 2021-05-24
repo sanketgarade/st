a8dc1d216d05d8f3a0088788c75c47e81b3466f2
संकेत गराडे <garade@pm.me>
2021-05-24 17:11:39 +0900

    clipboard patch on 0.8.3
    https://st.suckless.org/patches/clipboard/st-clipboard-0.8.3.diff
    ---
    one clipboard
    The Freedesktop standard requires you to remember which clipboard you
    are keeping selections in. If you switch between a terminal and browser,
    you may find this UX jarring.
    
    http://standards.freedesktop.org/clipboards-spec/clipboards-latest.txt
    
    ---
    Description
    This trivial patch sets CLIPBOARD on selection, the same as your
    browser.
    
    You may want to replace selpaste with clippaste in your config.h
    bindings to complete the affect.

--- config.def.h
+++ config.def.h
@@ -165,7 +165,7 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ XK_ANY_MOD,           Button2, clippaste,       {.i = 0},      1 },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
@@ -185,8 +185,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_Y,           clippaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      clippaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
